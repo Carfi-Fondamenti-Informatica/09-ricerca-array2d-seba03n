411e4d7d3234e4a7da7c365e3a9dfc0d64c65c58
seba03n <115528575+seba03n@users.noreply.github.com>
2022-12-17 11:09:03 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,27 @@
 #include <iostream>
+#include "lib.h"
+using namespace std;
 
-int main(){
-  char a[10][20];
-    int i;
-    for(i = 0; i < 10; i++){
-        cin >> a[i];
+int main() {
+    char list[10][20];
+    char name[20];
+    int p = 0;
+
+    for(int i=0;i<10;i++){
+        for(int j=0;j<20;j++) {
+            name[j] = ' ';
+            list[i][j] = ' ';
+        }
     }
-    char b[20];
-    cin >> b;
-    listanomi(a,b);
-  return 0;
+    for(int i=0;i<10;i++){
+        cin >> list[i];
+    }
+
+    cin >> name;
+    if(comp(list,p,name)){
+        cout << p;
+    }else{
+        cout << "non presente";
+    }
+    return 0;
 }
