4021bebaf736ec036c9aa13a5fcd8ed20d88a8b6
seba03n <115528575+seba03n@users.noreply.github.com>
2022-12-17 11:06:13 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,1 +1,5 @@
-void listanomi(char a[5][20], char b[20]);
+#ifndef ESERCITAZIONE9_2_LIB_H
+#define ESERCITAZIONE9_2_LIB_H
+bool comp(char [10][20], int &, char[]);
+#endif //ESERCITAZIONE9_2_LIB_H
+
