eb65a338ef3c5d193f2073ca936999105d9746bb
seba03n <115528575+seba03n@users.noreply.github.com>
2022-12-17 10:32:56 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,13 @@
 #include <iostream>
 
 int main(){
+  char a[10][20];
+    int i;
+    for(i = 0; i < 10; i++){
+        cin >> a[i];
+    }
+    char b[20];
+    cin >> b;
+    listanomi(a,b);
   return 0;
 }
